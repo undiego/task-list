aad3e6e349cb24be215a138d630db958b621fcbd
Diego Barrio <barriodiego@gmail.com>
2022-02-02 18:35:51 -0300

    Se agrega función para borrar tarea

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -17,4 +17,8 @@ export class TaskService {
     //return tasks;
     return this.http.get<Task[]>(this.apiUrl);
   }
+  deleteTask(task:Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
 }
