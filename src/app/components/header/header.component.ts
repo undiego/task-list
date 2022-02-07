6b8bbd7b7f0ecd1c55444d1dcfd20f5990424205
Diego Barrio <barriodiego@gmail.com>
2022-02-06 22:00:33 -0300

    Se agrega toggle boton agregar tarea junto a display form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs'; 
 
 @Component({
   selector: 'app-header',
@@ -8,14 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title:string = 'Lista de tareas';
+  showAddTask: boolean = true;
+  subscription?: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
   }
   
   toggleAddTask(){
-    console.log("Toggle");
+    //console.log("Toggle");
+    this.uiService.toggleAddTask();
   }
 
 }
